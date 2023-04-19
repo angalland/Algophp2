be2c6408dbf6d402666f52945c107c9cf29deb76
angalland <angalland@laposte.net>
2023-04-19 13:34:30 +0200

    exo 11 ajout constant pre-defini

--- exo11.php
+++ exo11.php
@@ -9,46 +9,26 @@ formaterDateFr("2018-02-23");
 <h2>Resultat</h2>
 
 <?php
+
+
 /*$date = DateTimeImmutable::createFromFormat ('Y-m-d', '2018-02-23');
-echo $date->format('l d F Y');*/
-
-/*$formatter = new IntlDateformatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
-echo $formatter->format(time('l d F Y'));*/
-/*function formaterDateFr($date){
-       DateTimeImmutable::createFromFormat('U', time());
-       echo $date->format('l d F Y');
-}
-
-/*function formaterDateFr($date) {
-       
-       $date = DateTimeImmutable::createFromFormat ('U', time()); // assigne la methode createFromFormat
-       $result = $date->format('l d F Y');
-       //var_dump($result);
-       
-       /* tableau des jours et mois en anglais et français
-       
-       $english_days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
-       $french_days = array('lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche');
-       $english_months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
-       $french_months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
-       
-       Remplace les mots anglais par les mots français
-       str_replace($english_days, $french_days, $result->l);
-       str_replace($english_months, $french_months, $result->F);
-       
-       return $result;
-};*/
+echo $date->format('l d F Y'); Renvoie la date en lettre mais en anglais*/
 
 
-$date ="1980-05-12";
+$date ="2018-02-23";
+
 function formaterDateFr($date){
-       
-       
-       $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+       $formatter = new IntlDateFormatter('fr_FR',     IntlDateFormatter::RELATIVE_FULL, IntlDateFormatter::NONE);
+
        echo $formatter->format(date_create($date))."</br>";
-}
+};
 
 echo formaterDateFr($date);
+       
+
+
+
+
 
 
 
--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 require_once ("exo13.php");
 
 $v1 = new Voiture ("Peugeot","408",5, 50);
-echo $v1;
+echo $v1->getMarque();
+var_dump($v1->getMarque());
