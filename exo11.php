4de2e469e9801b0d10adcb6e4b0506ce04048737
angalland <angalland@laposte.net>
2023-04-19 11:01:00 +0200

    ajout fonction IntlDateFormatter

--- exo11.php
+++ exo11.php
@@ -12,30 +12,43 @@ formaterDateFr("2018-02-23");
 /*$date = DateTimeImmutable::createFromFormat ('Y-m-d', '2018-02-23');
 echo $date->format('l d F Y');*/
 
+/*$formatter = new IntlDateformatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+echo $formatter->format(time('l d F Y'));*/
+/*function formaterDateFr($date){
+       DateTimeImmutable::createFromFormat('U', time());
+       echo $date->format('l d F Y');
+}
 
-
-function formaterDateFr($date) {
-       $date = DateTimeImmutable::createFromFormat ('Y-m-d', '2018-02-23'); // assigne la methode createFromFormat
+/*function formaterDateFr($date) {
+       
+       $date = DateTimeImmutable::createFromFormat ('U', time()); // assigne la methode createFromFormat
        $result = $date->format('l d F Y');
        //var_dump($result);
-
-       // tableau des jours et mois en anglais et français
+       
+       /* tableau des jours et mois en anglais et français
        
        $english_days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
        $french_days = array('lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche');
        $english_months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
        $french_months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
+       
+       Remplace les mots anglais par les mots français
+       str_replace($english_days, $french_days, $result->l);
+       str_replace($english_months, $french_months, $result->F);
+       
+       return $result;
+};*/
 
-       // Remplace les mots anglais par les mots français
-        str_replace($english_days, $french_days, $result->l);
-        str_replace($english_months, $french_months, $result->F);
 
-        return $result;
-};
+$formatter ="2018-02-23";
+function formaterDateFr($formatter){
+       
+       
+       $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+       echo $formatter->format(time())."</br>";
+}
 
-echo formaterDateFr('2018-02-23');
-
-echo $date->format('l');
+echo formaterDateFr($formatter);
 
 
 
