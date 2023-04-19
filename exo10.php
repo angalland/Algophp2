eed12f9cb0e7d2432c6f4380b06118a4d527eba4
angalland <angalland@laposte.net>
2023-04-19 09:56:38 +0200

    exo10php mise en place que d'une seul balise form

--- exo10.php
+++ exo10.php
@@ -11,43 +11,43 @@ de validation (submit).</p>
 <?php
 
 $nomsImput = array("Nom", "Prenom", "adresse e-mail", "Ville", "Sexe");
+$elements = array("Développeur Logiciel", "Designer Web", "Intégrateur", "Chef de projet");
+$validation = array ("Validation");
 
-function afficherInput($nomsImput){
+function afficherInput($nomsImput, $elements, $validation){
         $result = "<form action='' method='post'>";
     foreach ($nomsImput as $formulaire){
-        $result .= "<form action='' method='post'>
-                        <div>
-                            <label for='name'>".$formulaire."</label>
-                            </br>
-                            <input type='text' id='name'>
-                        </div>";                 
-    }
-    return $result;
-};
-
-echo afficherInput($nomsImput);
-echo "</br>";
-$elements = array("Développeur Logiciel", "Designer Web", "Intégrateur", "Chef de projet");
-
-function alimenterListeDeroulante($elements) {
-    $result = "<form action='#' method='post'>
-        <select name='civilite' id='civ'>";
+        $result .= "<div>
+                        <label for='name'>".$formulaire."</label>
+                        </br>
+                        <input type='text' id='name'>
+                    </div></br>";
+                };
+                 
+    $result .= "<select name='civilite' id='civ'>";
     foreach ($elements as $element) {
         $result.= "<option value='".$element."'>".$element."</option>";
     };
-    $result.="</select></form>";
-    return $result;
-};
+    $result.="</select></br></br>";
 
-$validation = array ("Validation");
-
-function boutonValider($validation){
     foreach ($validation as $valider){
-        $result = "<input type='submit' value='".$valider."'>";
+        $result .= "<input type='submit' value='".$valider."'>";
     };
+    $result.="</form>";
     return $result;
+
 };
 
-echo alimenterListeDeroulante($elements);
-echo "</br>";
-echo boutonValider($validation);
+
+
+
+echo afficherInput($nomsImput, $elements, $validation);
+
+
+//function alimenterListeDeroulante($elements) {
+//    $result = "<form action='#' method='post'>
+
+
+//function boutonValider($validation){
+
+
