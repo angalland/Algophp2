a109c752dc5d772d4ed3a6e7094ee40f9e9a7fe8
angalland <angalland@laposte.net>
2023-04-19 09:36:30 +0200

    ajout exo 11 convertir format date

--- exo11.php
+++ exo11.php
@@ -9,12 +9,37 @@ formaterDateFr("2018-02-23");
 <h2>Resultat</h2>
 
 <?php
+/*$date = DateTimeImmutable::createFromFormat ('Y-m-d', '2018-02-23');
+echo $date->format('l d F Y');*/
 
-$date = new DateTime("2018-02-22");
+
 
 function formaterDateFr($date) {
-       $date = DateTimeImmutable::createFromFormat('Y-m-d', "$date");
-       return  date->format('D-M-Y');
+       $date = DateTimeImmutable::createFromFormat ('Y-m-d', '2018-02-23'); // assigne la methode createFromFormat
+       $result = $date->format('l d F Y');
+       //var_dump($result);
+
+       // tableau des jours et mois en anglais et français
+       
+       $english_days = array('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+       $french_days = array('lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche');
+       $english_months = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+       $french_months = array('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
+
+       // Remplace les mots anglais par les mots français
+        str_replace($english_days, $french_days, $result->l);
+        str_replace($english_months, $french_months, $result->F);
+
+        return $result;
 };
 
-echo formaterDateFr($date);
+echo formaterDateFr('2018-02-23');
+
+echo $date->format('l');
+
+
+
+
+
+
+
