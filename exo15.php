d325c30f51301c1f093206bfa5b58aac42f57a01
angalland <angalland@laposte.net>
2023-04-20 13:51:23 +0200

    exo 15 ajout de la fonction validerEmail

--- exo15.php
+++ exo15.php
@@ -6,9 +6,16 @@ adresse e-mail a le bon format.</p>
 <h2>Résultat</h2>
 
 <?php
+$email = "elan@elan-formation.fr";
 
-function validerEmail(){
-    $result = "<form action='' method='post'>
-                <label for='name'>;
-    
-}
+function validerEmail($email){
+    if (filter_var($email, FILTER_VALIDATE_EMAIL) == true){ 
+        return "L'adresse ".$email." est une adresse e-mail valide.";
+    } else {
+        return "L'adresse ".$email." est une adresse e-mail invalide";
+    }
+};
+
+echo validerEmail($email);
+
+// la fonction filter_var (string $email, FILTER_VALIDATE_EMAIL) renvoie true si l'adressse email est valide et false si elle est invalide 
