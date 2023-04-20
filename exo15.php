0a2b6bfdcd6401c32322e580a45ac90329a7bc51
angalland <angalland@laposte.net>
2023-04-20 12:00:50 +0200

    ajout exo15

---
+++ exo15.php
@@ -0,0 +1,14 @@
+<h1>Exercice 15</h1>
+
+<p>En utilisant les ressources de la page http://php.net/manual/fr/book.filter.php, vérifier si une 
+adresse e-mail a le bon format.</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+function validerEmail(){
+    $result = "<form action='' method='post'>
+                <label for='name'>;
+    
+}
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // require_once ("exo13.php");
-require_once ("exo14.php");
-
+// require_once ("exo14.php");
+require_once ("exo15.php");
 
 
 
