6e213f3a25c95cd62cf0e2e55afeb3972a6c5283
angalland <angalland@laposte.net>
2023-04-20 11:35:36 +0200

    exo13 modification index.php

--- exo13.php
+++ exo13.php
@@ -164,6 +164,58 @@ Class Voiture{
     }
     
 }
+
+
+/*echo $v1->getMarque(); la méthode get permet d'appeler l'attribut
+echo $v1->getModele();
+echo $v1->getNbportes();
+echo $v1->getVitesseActuelle();
+
+/*$v1->setMarque("citroen"); la methode set permet de modiffier l'attribut
+echo $v1->getMarque();*/
+
+//  ------------------------ exo 13 -------------------------------
+
+$v1 = new Voiture("Peugeot", "408",5);
+$v2 = new Voiture("Citroën", "C4", 3);
+
+// ----------------------Véhicule 1 -----------------
+$v1->setDemarrer(1);
+$v1->demarrerVoiture1();
+
+echo "</br>";
+
+$v1->setVitesseActuelle(30);
+$v1->accelererVehicule();
+
+echo "</br>";
+
+// ------------------------véhicule 2--------------
+
+$v2->setDemarrer(1);
+$v2->demarrerVoiture1();
+
+echo"</br>";
+
+$v2->setVitesseActuelle(20);
+$v2->accelererVehicule();
+
+echo"</br>";
+
+echo $v1->vitesseVehicule();
+echo $v2->vitesseVehicule();
+
+echo"</br>";
+echo"</br>";
+
+// --------------------------- information véhicule -------------
+
+echo $v1->informationVehicule();
+
+echo"</br>";
+echo"</br>";
+
+echo $v2->informationVehicule();
     
     
     
--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-
-require_once ("exo13.php");
-require_once ("exo14.php");
+ require_once ("exo13.php");
+/*require_once ("exo14.php");
 
 
 /*echo $v1->getMarque(); la méthode get permet d'appeler l'attribut
 echo $v1->getModele();
 echo $v1->getNbportes();
-echo $v1->getVitesseActuelle();*/
+echo $v1->getVitesseActuelle();
 
 /*$v1->setMarque("citroen"); la methode set permet de modiffier l'attribut
 echo $v1->getMarque();*/
@@ -15,77 +14,13 @@ echo $v1->getMarque();*/
 
 
 
+//  -------------------------------- exo 14 ---------------------------
 
+$v1 = new Voitures ("Peugeot", "408");
+$ve1 = new Voitures ("BMW", "I3", 100);
 
-
-
-/* Véhicule 1
-$v1 = new Voiture("Peugeot", "408",5);
-$v1->setDemarrer(1);
-$v1->demarrerVoiture();
-$v1->setacceleration(20);
-// $v1->vitesseVehicule();
-$v1->accelererVehicule();
-// $v1->setVitesseActuelle(50);
-echo $v1->informationVehicule();
-$v1->demarrerVoiture1();
-echo "</br>";
-echo "</br>";
-echo $v1->vitesseVehicule();
-
-echo "</br></br></br>";
-
-// Vehicule 2
-$v2 = new Voiture("Citroën", "C4", 3);
-$v2->setDemarrer(0);
-$v2->demarrerVoiture();
-$v2->setVitesseActuelle(0);
-$v2->vitesseVehicule();
-echo $v2->informationVehicule();
-$v2->demarrerVoiture1();
-echo "<br>";
-$v2->setacceleration(20);
-$v2->accelererVehicule();*/
-
-$v1 = new Voiture("Peugeot", "408",5);
-$v2 = new Voiture("Citroën", "C4", 3);
-
-// ----------------------Véhicule 1 -----------------
-$v1->setDemarrer(1);
-$v1->demarrerVoiture1();
-
-echo "</br>";
-
-$v1->setVitesseActuelle(30);
-$v1->accelererVehicule();
-
-echo "</br>";
-
-// ------------------------véhicule 2--------------
-
-$v2->setDemarrer(1);
-$v2->demarrerVoiture1();
-
-echo"</br>";
-
-$v2->setVitesseActuelle(20);
-$v2->accelererVehicule();
-
-echo"</br>";
-
-echo $v1->vitesseVehicule();
-echo $v2->vitesseVehicule();
-
-echo"</br>";
-echo"</br>";
-
-echo $v1->informationVehicule();
-
-echo"</br>";
-echo"</br>";
-
-echo $v2->informationVehicule();
-
+echo $v1->getInfos()."</br>";
+echo $ve1->getInfos()."</br>";*/
 
 
 
